dc7b9b9fc6331490b93df41173cf8dd02c756f07
Anton <anton.gorshenin27@gmail.com>
2023-03-06 14:48:25 -0800

    Working on docker prod

--- fastapi_app/app/s3.py
+++ fastapi_app/app/s3.py
@@ -5,12 +5,15 @@ from .config import settings
 
 class S3:
 	def __init__(self):
-		self.s3 = boto3.client('s3', 
-			aws_access_key_id=settings.S3_ACCESS_KEY, 
-			aws_secret_access_key=settings.S3_SECRET_KEY,
-			region_name=settings.S3_REGION,
-			endpoint_url=settings.S3_HOST_NAME
-		)
+		if settings.DEV:
+			self.s3 = boto3.client('s3', 
+				aws_access_key_id=settings.S3_ACCESS_KEY, 
+				aws_secret_access_key=settings.S3_SECRET_KEY,
+				region_name=settings.S3_REGION,
+				endpoint_url=settings.S3_HOST_NAME
+			)
+		else:
+			self.s3 = boto3.client('s3', aws_access_key_id=settings.S3_ACCESS_KEY, aws_secret_access_key=settings.S3_SECRET_KEY)
 
 	def add_new_photo(self, file, key):
 		try:
